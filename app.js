09ec8398f18f83f12b5a6d36dbe289719b756499
Brown <keithw.brown@dana.com>
2023-03-28 14:08:09 -0400

    Add EJS package

--- app.js
+++ app.js
@@ -3,14 +3,39 @@ const bodyparser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
   let today = new Date();
+  let currentDay = today.getDay();
+  let day = "";
 
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    res.send("Hope you're having a good weekend!");
-  } else {
-    res.send("Here's hoping the work week goes quickly for you!");
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log(currentDay);
   }
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
