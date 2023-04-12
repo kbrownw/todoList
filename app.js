11921afd64488433837b2397f9db584604a6c199
Keith Brown <kbrownw@gmail.com>
2023-04-12 08:57:58 -0400

    Add new items to list functionality

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
 const express = require("express");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
+let items = [];
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   let today = new Date();
-  let currentDay = today.getDay();
-  let day = "";
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  let day = today.toLocaleDateString("en-US", options);
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log(currentDay);
-  }
-  res.render("list", { kindOfDay: day });
+  res.render("list", { kindOfDay: day, itemArray: items });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.additem;
+
+  items.push(item);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
