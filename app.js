579216a5c7a22d36a465bbe45e18c4c2f836b2d5
Keith Brown <kbrownw@gmail.com>
2023-04-14 11:37:30 -0400

    Add about page and separete date function into the date.js module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 let items = [];
@@ -10,14 +11,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  let day = today.toLocaleDateString("en-US", options);
-
+  let day = date.getDate();
   res.render("list", { listTitle: day, itemArray: items });
 });
 
@@ -35,6 +29,10 @@ app.post("/", function (req, res) {
 
 app.get("/work", function (req, res) {
   res.render("list", { listTitle: "Work List", itemArray: workItems });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
 app.listen(3000, function () {
